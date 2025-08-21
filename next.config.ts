4db28ce47f00b9fee3cb3bf80347bbdef31b8dda
Alex Pezzini <alex.pezzini@intesys.it>
2025-08-21 14:55:11 +0200

    refactor: migrate configuration from next.config.js to next.config.ts and update tsconfig.json

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
+
+const nextConfig: NextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
+
+const withNextIntl = createNextIntlPlugin();
+export default withNextIntl(nextConfig);
