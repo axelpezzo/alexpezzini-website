b9458fe7afaf1d69891c1822fa00a4cc5d7f89be
Alex Pezzini <alex.pezzini@intesys.it>
2025-08-21 15:19:02 +0200

    fix: update deploy workflow to use 'next build' instead of 'next export' and set output in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
+  output: "export",
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
